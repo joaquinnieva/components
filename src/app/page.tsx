0683f3971d428b92b85d8d4c94a201f99f67e686
Joaquin Nieva <jnieva@boxcustodia.com>
2023-12-04 09:00:48 -0300

    Deje componente de dropdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,13 +16,15 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      {/* <Pie /> */}
-      {/* <Select label="Label" options={['Opcion 1', 'Opcion 2', 'Opcion 3', 'Opcion 4']} onSelect={setSelected} selected={selected} /> */}
-      {/* <Input onlyNumbers={true} value={text} onChange={setText} noAvailableWords={['k', 'l']} charToAdd="-" addCharIn={[2, 11]} max={13} /> */}
-      {/* <TestLive /> */}
-      {/* <LoadingManager action={promisee} /> */}
-      {/* <Image /> */}
-      <Menu />
+      <div className="w-80">
+        {/* <Pie /> */}
+        {/* <Select label="Label" options={['Opcion 1', 'Opcion 2', 'Opcion 3', 'Opcion 4']} onSelect={setSelected} selected={selected} /> */}
+        {/* <Input onlyNumbers={true} value={text} onChange={setText} noAvailableWords={['k', 'l']} charToAdd="-" addCharIn={[2, 11]} max={13} /> */}
+        {/* <TestLive /> */}
+        {/* <LoadingManager action={promisee} /> */}
+        {/* <Image /> */}
+        <Menu options={[{ name: 'asd1' }, { name: 'basd2' }, { name: 'casd3' }, { name: 'asd4' }]} />
+      </div>
     </main>
   );
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,29 +1,81 @@
-import useClickOutside from '@/hooks/useClickOutside';
+import ArrowDropdown from '@/icons/ArrowDropdown';
 import { useState } from 'react';
 
-function Menu() {
-  const [show, setShow] = useState(false);
-  const select = useClickOutside((): void => setShow(false));
+function Menu({ options, value = { name: 'Seleccione' }, onSelect, optionKey = 'name' }: { options: any[]; value?: any; onSelect?: Function; optionKey?: string }) {
+  const [show, setShow] = useState<boolean | undefined>(undefined);
+  const [selected, setSelected] = useState(value);
+  const [preSelectedIndex, setPreSelectedIndex] = useState(-1);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [clicks, setClicks] = useState(0);
+
+  const handleSelect = (option: any, index: number) => {
+    onSelect?.(option);
+    setSelected(option);
+    setSelectedIndex(index);
+    setPreSelectedIndex(index);
+  };
+
+  const onKeyDown = (e: any): void => {
+    if (e.key === 'Tab' || e.key === 'Escape') {
+      setPreSelectedIndex(selectedIndex);
+      e?.target?.blur();
+      return;
+    }
+    if (e.key === 'Enter') {
+      handleSelect(options?.[preSelectedIndex], preSelectedIndex);
+      e?.target?.blur();
+      return;
+    }
+    if (e.key === 'ArrowUp') {
+      setPreSelectedIndex((i: number): number => (i > 0 ? i - 1 : i));
+      return;
+    }
+    if (e.key === 'ArrowDown') {
+      setPreSelectedIndex((i: number): number => (i < options?.length - 1 ? i + 1 : i));
+      return;
+    }
+
+    const newIndex = options?.findIndex((option: any): boolean => {
+      return option?.[optionKey]?.[0].toLowerCase() === e.key.toLowerCase();
+    });
+    newIndex >= 0 && setPreSelectedIndex(newIndex);
+  };
+
+  const onMouseUp = (e: any): void => {
+    setClicks((e: number): number => e + 1);
+    if (clicks > 0) e.target.blur();
+  };
+  const onFocus = (): void => {
+    setShow(true);
+    setClicks(0);
+  };
+  const onBlur = (): void => {
+    setShow(false);
+    setClicks(0);
+  };
+
   return (
-    <div onClick={(e) => e.stopPropagation()}>
+    <div className="w-full">
       <button
-        onClick={() => setShow(!show)}
-        className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
-        label
+        onKeyDown={onKeyDown}
+        onMouseUp={onMouseUp}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        className="flex justify-start relative rounded-md text-sm font-medium transition-colors border border-input outline-gray-500 focus:outline-1 focus-visible:outline-1 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 w-full">
+        {selected?.[optionKey]}
+        <ArrowDropdown
+          className={`absolute top-1/2 right-2 transform ease-in -translate-x-1/2 -translate-y-1/2 
+        ${show ? 'rotate-180' : 'rotate-0'}`}
+        />
       </button>
-      <div
-        ref={select}
-        className={`z-50 mt-1 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 w-80 ${
-          show ? '' : 'invisible'
-        }`}>
-        <ul>
-          <li className="hover:bg-slate-100">1asd</li>
-          <li className="hover:bg-slate-100">2asd</li>
-          <li className="hover:bg-slate-100">
-            <input type="text" />
+
+      <ul className={`z-50 mt-1 py-1 rounded-md border shadow-md outline-none w-full ${show === true ? 'animation-drop-open' : show === false ? 'animation-drop-close' : 'invisible'}`}>
+        {options?.map((option: any, i: number) => (
+          <li key={i} className={`p-2 hover:bg-slate-100 ${i === preSelectedIndex ? 'bg-slate-100' : ''}`} onMouseDown={(): void => handleSelect(option, i)}>
+            {option?.[optionKey]}
           </li>
-        </ul>
-      </div>
+        ))}
+      </ul>
     </div>
   );
 }
